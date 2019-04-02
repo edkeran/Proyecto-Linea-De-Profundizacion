31c9a33744bf68c33b7b910a3ba10370d1b34493
Edgar <edgarkrejci12345@gmail.com>
2019-04-02 00:58:09 -0500

    Mejoras Proyecto

--- Controller/registroController.php
+++ Controller/registroController.php
@@ -8,7 +8,11 @@ require_once ('../Utilitarios/Util.php');
  * Controlador Para Manejar La Logica Del Registro
  */
 class registroController{
-    function registrar(){
+
+    /**
+     * Funcion Para Crear Un Nuevo Usuario En La Base De Datos
+     */
+    public function registrar(){
         //Se Obtienen Las Variables Enviadas Por El Formulario
          $nombre = $_POST['nombre'];
          $correo = $_POST['correo'];
@@ -18,8 +22,9 @@ class registroController{
          $nombreUsuario= $_POST['nmUsr'];
          //Se Validan Las Credenciales Ingresadas
          if ($contrasena = $confirmarContrasena){
-            
+            //Se Instancia Un Objeto Tipo Usuario
             $nuevoUsuario = new Usuario();
+            //Se Le Setean Sus Parametros
             $nuevoUsuario->setNombre($nombre);
             $nuevoUsuario->setApellido($apellido);
             $contrasena = Util::encriptarPassword($contrasena);
@@ -27,10 +32,24 @@ class registroController{
             $nuevoUsuario->setEmail($correo);
             $nuevoUsuario->setUsrName($nombreUsuario);
             $daoUsuario = new DaoUser();
+            //Se Crea El Usuario En La Base De Datos 
             $daoUsuario->create($nuevoUsuario);
          }
     }
 
+    public function loginUsuario(){
+        $nombreUsuario = $_POST['userName'];
+        $contrasenaUsuario = $_POST['pass'];
+
+    }
+
+    /**
+     *Metodo Para Obtener La Contraseña Encriptada Y Asi Realizar Su Respectiva Validacion
+     */
+    private function getPassdb(){
+        $daoUsuario  = new DaoUser();
+        
+    }
     
 
 }
