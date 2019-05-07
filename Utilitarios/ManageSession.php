5bfa2011939ef2d17b1e1d444820034451124871
Edgar <edgarkrejci12345@gmail.com>
2019-05-07 18:31:29 -0500

    Insercion Archivos Perdidos Por Merge Anterior

--- Utilitarios/ManageSession.php
+++ Utilitarios/ManageSession.php
@@ -12,4 +12,26 @@
         header("Location: ../View/web/index.php");
     }
 
+    /**
+     * Metodo No Estatico Para Validar La Sesion Para Limitar Las Paginas Segun Sea El Caso
+     */
+    public function validateSession(){
+        session_start();
+        if(isset($_SESSION['cliente'])){
+           echo 'Hola Mundo';
+        }else{
+            if(isset($_SESSION['vendedor'])){
+                echo 'Me Muero';
+            }else{
+                if(isset($_SESSION['administrador'])){
+                    echo 'Averiguo';
+                }
+            }
+        }
+
+    }
+
+
+
+
  }
